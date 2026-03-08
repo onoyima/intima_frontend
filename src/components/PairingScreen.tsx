aadafc1167491460a88adeb16fe1b62c1f9ae181
onoyima <clintonfaze@outlook.com>
2026-03-08 13:47:23 +0100

    session

--- src/components/PairingScreen.tsx
+++ src/components/PairingScreen.tsx
@@ -21,6 +21,7 @@ export function PairingScreen() {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ inviteCode: code }),
+        credentials: "include",
       });
       if (!res.ok) {
         const error = await res.json();
